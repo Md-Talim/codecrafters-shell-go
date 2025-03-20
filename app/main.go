8cd6beaf6cef7b9649116c5f8cb463a5a46bd237
Mohammad Talim <talimmohammad116@gmail.com>
2025-03-20 04:32:48 +0000

    handle invalid commands

--- app/main.go
+++ app/main.go
@@ -10,5 +10,12 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+        fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+		os.Exit(0)
+	}
+
+	// Command not found
+	fmt.Printf("%s: command not found", command[:len(command)-1])
 }
